e3be047a69f72ce65d980ec31e9063c4ff1b3bd2
Heidi4 <erickkaloki28@gmail.com>
2024-03-05 18:31:35 +0300

    Working with CSS react modules for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import InlineComponent from "./components/InlineComponent"
-import Todo from "./components/Todo"
+import InlineComponent from "./components/InlineComponent";
+import OutLine from "./components/OutLine";
+import Todo from "./components/Todo";
 function App() {
-  return  <div className="App">
-    <InlineComponent></InlineComponent>
-    {/* <Todo /> */}
-  </div>
+  return (
+    <div className="App">
+      <InlineComponent></InlineComponent>
+      <OutLine />
+      {/* <Todo /> */}
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/OutLine.jsx
@@ -0,0 +1,11 @@
+import styles from "./outline.module.css";
+
+function OutLine() {
+  return (
+    <div>
+      <h1 className={styles.header}>This is an outline component</h1>
+    </div>
+  );
+}
+
+export default OutLine;
