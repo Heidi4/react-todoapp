63a1ff31d099484611456b72c2011ad7344b0ac6
Heidi4 <erickkaloki28@gmail.com>
2024-03-05 20:24:10 +0300

    Making the delete interactive

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -7,7 +7,7 @@ function Todo() {
   return (
     <div>
       <Form todos={todos} setTodos={setTodos}/>
-      <TodoList todos={todos}/>
+      <TodoList todos={todos} setTodos={setTodos}/>
     </div>
   );
 }
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 import styles from "./todoitem.module.css";
-function TodoItem({ item }) {
+function TodoItem({ item, todos, setTodos }) {
+  function handleDelete(item) {
+    console.log("Deleted", item);
+    setTodos(todos.filter((todo) => todo !== item)
+    )
+    
+  }
   return (
     <div className={styles.item}>
-      <div className={styles.itemName}>{item}</div>
+      <div className={styles.itemName}>
+        {item}
+        <span>
+          <button
+            className={styles.deleteBtn}
+            onClick={() => handleDelete(item)}
+          >
+            x
+          </button>
+        </span>
+      </div>
+
       <hr className={styles.line} />
     </div>
   );
